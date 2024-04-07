5620e78d3d00bfa8ac8ea1897aaf45782a1f7658
Yongbi21 <111903577+Yongbi21@users.noreply.github.com>
2024-04-07 19:36:41 +0800

    header-footer-and-login-page

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,38 @@ module.exports = {
     './node_modules/flowbite/**/*.js'
 ],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      'blue': '#1fb6ff',
+      'purple': '#7e5bef',
+      'pink': '#ff49db',
+      'orange': '#ff7849',
+      'green': '#13ce66',
+      'yellow': '#ffc82c',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6',
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    borderRadius: {
+      '4xl': '2rem',
+    },
+    extend: {
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+    },
   },
+  
   plugins: [require('flowbite/plugin')],
 }
 
+}
