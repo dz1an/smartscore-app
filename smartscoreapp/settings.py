7aea271002bb01d6f051a7c7b31afc17640f4ffa
kent <dzian2k17@gmail.com>
2024-09-25 10:26:26 +0800

    allowed host

--- smartscoreapp/settings.py
+++ smartscoreapp/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-5y6@@b^mt-)(=*q0n3pc0jtfa)(u#owmhtjq%+_6&wk5uvami1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['smartscoreapp.test', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
